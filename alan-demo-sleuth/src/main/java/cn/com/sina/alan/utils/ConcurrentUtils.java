3fc210490f8ac10a8ee0d451258f13e2ffa6fc80
wanghongfei <brucewhf@gmail.com>
2016-09-28 10:47:56 +0800

    并发工具类修改

--- alan-demo-sleuth/src/main/java/cn/com/sina/alan/utils/ConcurrentUtils.java
+++ alan-demo-sleuth/src/main/java/cn/com/sina/alan/utils/ConcurrentUtils.java
@@ -1,26 +1,19 @@
 package cn.com.sina.alan.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * 并发执行任务工具类
- * Created by 王鸿飞 (hongfei7@staff.sina.com.cn)  
- * 8/27/16.
+ * Created by wanghongfei(hongfei7@staff.sina.com.cn) on 8/27/16.
  */
 public class ConcurrentUtils {
     private static Logger log = LoggerFactory.getLogger(ConcurrentUtils.class);
@@ -51,11 +44,22 @@ public class ConcurrentUtils {
      * @param task1 并发任务1
      * @param task2 并发任务2
      * @param <T> 并发调用返回结果的类型
-     * @throws Exception 并发调用发生错误
      * @return
      */
     public static <T> List<T> concurrentExecuteSame(AlanTask<T> task1, AlanTask<T> task2) throws Exception {
-        return concurrentExecuteSame(task1, task2, TIMEOUT, UNIT);
+        return concurrentExecuteSame(task1, task2, TIMEOUT, UNIT, null);
+    }
+
+    /**
+     * 并发执行两个返回结果相同的任务
+     * @param task1 并发任务1
+     * @param task2 并发任务2
+     * @param <T> 并发调用返回结果的类型
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
+     * @return
+     */
+    public static <T> List<T> concurrentExecuteSame(AlanTask<T> task1, AlanTask<T> task2, ExecutorService executorService) throws Exception {
+        return concurrentExecuteSame(task1, task2, TIMEOUT, UNIT, executorService);
     }
 
     /**
@@ -67,10 +71,24 @@ public class ConcurrentUtils {
      * @param <T1> 并发任务返回结果对象的类型
      * @param <T2> 聚合后的返回类型
      * @return
-     * @throws Exception
      */
     public static <T1, T2> T2 concurrentExecuteSame(AlanTask<T1> task1, AlanTask<T1> task2, BiFunction<T1, T1, T2> func) throws Exception {
-        return concurrentExecuteSame(task1, task2, func, TIMEOUT, UNIT);
+        return concurrentExecuteSame(task1, task2, func, TIMEOUT, UNIT, null);
+    }
+
+    /**
+     * 并发执行两个任务,并调用用户自定义逻辑聚合结果
+     *
+     * @param task1 并发任务1
+     * @param task2 并发任务2
+     * @param func 聚合逻辑
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
+     * @param <T1> 并发任务返回结果对象的类型
+     * @param <T2> 聚合后的返回类型
+     * @return
+     */
+    public static <T1, T2> T2 concurrentExecuteSame(AlanTask<T1> task1, AlanTask<T1> task2, BiFunction<T1, T1, T2> func, ExecutorService executorService) throws Exception {
+        return concurrentExecuteSame(task1, task2, func, TIMEOUT, UNIT, executorService);
     }
 
 
@@ -80,17 +98,17 @@ public class ConcurrentUtils {
      * @param task2 并发任务2
      * @param timeout 超时时间值
      * @param unit 超时时间值的单位
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
      * @param <T>
-     * @throws Exception 并发调用发生错误
      * @return
      */
-    public static <T> List<T> concurrentExecuteSame(AlanTask<T> task1, AlanTask<T> task2, int timeout, TimeUnit unit) throws Exception {
+    public static <T> List<T> concurrentExecuteSame(AlanTask<T> task1, AlanTask<T> task2, int timeout, TimeUnit unit, ExecutorService executorService) throws Exception {
         Future<List<T>> f = CompletableFuture.supplyAsync(
                 () -> task1.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ).thenCombine( CompletableFuture.supplyAsync(
                 () -> task2.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ), (r1, r2) -> Arrays.asList(r1, r2));
 
         return processFuture(f, timeout, unit);
@@ -106,15 +124,14 @@ public class ConcurrentUtils {
      * @param <T1> task1的返回类型
      * @param <T2> task2的返回类型
      * @return
-     * @throws Exception
      */
-    public static <T1, T2> T2 concurrentExecuteSame(AlanTask<T1> task1, AlanTask<T1> task2, BiFunction<T1, T1, T2> func, int timeout, TimeUnit unit) throws Exception {
+    public static <T1, T2> T2 concurrentExecuteSame(AlanTask<T1> task1, AlanTask<T1> task2, BiFunction<T1, T1, T2> func, int timeout, TimeUnit unit, ExecutorService executorService) throws Exception {
         Future<T2> f = CompletableFuture.supplyAsync(
                 () -> task1.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ).thenCombine( CompletableFuture.supplyAsync(
                 () -> task2.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ), func);
 
         return processFuture(f, timeout, unit);
@@ -126,10 +143,21 @@ public class ConcurrentUtils {
      * @param tasks 并发任务
      * @param <T> 返回类型
      * @return
-     * @throws Exception
      */
     public static <T> T concurrentExecuteRace(AlanTask<T>... tasks) throws Exception {
-        return concurrentExecuteRace(TIMEOUT, UNIT, tasks);
+        return concurrentExecuteRace(TIMEOUT, UNIT, null, tasks);
+    }
+
+    /**
+     * 并发执行多个任务,取最快返回的结果
+     *
+     * @param tasks 并发任务
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
+     * @param <T> 返回类型
+     * @return
+     */
+    public static <T> T concurrentExecuteRace(ExecutorService executorService, AlanTask<T>... tasks) throws Exception {
+        return concurrentExecuteRace(TIMEOUT, UNIT, executorService, tasks);
     }
 
     /**
@@ -139,13 +167,12 @@ public class ConcurrentUtils {
      * @param tasks 并发任务
      * @param <T> 返回类型
      * @return
-     * @throws Exception
      */
-    public static <T> T concurrentExecuteRace(int timeout, TimeUnit unit, AlanTask<T>... tasks) throws Exception {
+    public static <T> T concurrentExecuteRace(int timeout, TimeUnit unit, ExecutorService executorService, AlanTask<T>... tasks) throws Exception {
         checkTasks(tasks);
 
         CompletableFuture<T>[] futures = Stream.of(tasks)
-                .map( task -> CompletableFuture.supplyAsync( () -> task.execute(), getPool() ) )
+                .map( task -> CompletableFuture.supplyAsync( () -> task.execute(), executorService == null ? getPool() : executorService ) )
                 .toArray( CompletableFuture[]::new );
 
         CompletableFuture f = CompletableFuture.anyOf(futures);
@@ -159,10 +186,22 @@ public class ConcurrentUtils {
      * @param <T1> task1的返回类型
      * @param <T2> task2的返回类型
      * @return
-     * @throws Exception
      */
     public static <T1, T2> List<Object> concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2) throws Exception {
-        return concurrentExecuteDiff(task1, task2, TIMEOUT, UNIT);
+        return concurrentExecuteDiff(task1, task2, TIMEOUT, UNIT, null);
+    }
+
+    /**
+     * 并发执行两个返回类型不同的任务
+     * @param task1 并发任务1
+     * @param task2 并发任务2
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
+     * @param <T1> task1的返回类型
+     * @param <T2> task2的返回类型
+     * @return
+     */
+    public static <T1, T2> List<Object> concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, ExecutorService executorService) throws Exception {
+        return concurrentExecuteDiff(task1, task2, TIMEOUT, UNIT, executorService);
     }
 
     /**
@@ -175,10 +214,25 @@ public class ConcurrentUtils {
      * @param <T2> task2的返回类型
      * @param <T3> 聚合结果后的返回类型
      * @return
-     * @throws Exception
      */
     public static <T1, T2, T3> T3 concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, BiFunction<T1, T2, T3> func) throws Exception {
-        return concurrentExecuteDiff(task1, task2, func, TIMEOUT, UNIT);
+        return concurrentExecuteDiff(task1, task2, func, TIMEOUT, UNIT, null);
+    }
+
+    /**
+     * 并发执行两个返回类型不同的任务, 并调用用户逻辑聚合结果
+     *
+     * @param task1 并发任务1
+     * @param task2 并发任务2
+     * @param func 聚合逻辑
+     * @param executorService 执行任务的线程池. 可传递null表示使用工具类默认的池
+     * @param <T1> task1的返回类型
+     * @param <T2> task2的返回类型
+     * @param <T3> 聚合结果后的返回类型
+     * @return
+     */
+    public static <T1, T2, T3> T3 concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, BiFunction<T1, T2, T3> func, ExecutorService executorService) throws Exception {
+        return concurrentExecuteDiff(task1, task2, func, TIMEOUT, UNIT, executorService);
     }
 
     /**
@@ -189,16 +243,15 @@ public class ConcurrentUtils {
      * @param unit 超时时间值的单位
      * @param <T1> task1的返回类型
      * @param <T2> task2的返回类型
-     * @throws Exception 并发调用发生错误
      * @return
      */
-    public static <T1, T2> List<Object> concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, int timeout, TimeUnit unit) throws Exception {
+    public static <T1, T2> List<Object> concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, int timeout, TimeUnit unit, ExecutorService executorService) throws Exception {
         Future<List<Object>> f = CompletableFuture.supplyAsync(
                 () -> task1.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ).thenCombine( CompletableFuture.supplyAsync(
                 () -> task2.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ), (r1, r2) -> {
             List<Object> result = new ArrayList<>(2);
             result.add(r1);
@@ -221,15 +274,14 @@ public class ConcurrentUtils {
      * @param <T2> 并发任务2的返回结果
      * @param <T3> 聚合结果
      * @return
-     * @throws Exception
      */
-    public static <T1, T2, T3> T3 concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, BiFunction<T1, T2, T3> func, int timeout, TimeUnit unit) throws Exception {
+    public static <T1, T2, T3> T3 concurrentExecuteDiff(AlanTask<T1> task1, AlanTask<T2> task2, BiFunction<T1, T2, T3> func, int timeout, TimeUnit unit, ExecutorService executorService) throws Exception {
         Future<T3> f = CompletableFuture.supplyAsync(
                 () -> task1.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ).thenCombine( CompletableFuture.supplyAsync(
                 () -> task2.execute(),
-                getPool()
+                executorService == null ? getPool() : executorService
         ), func);
 
         return processFuture(f, timeout, unit);
@@ -239,10 +291,18 @@ public class ConcurrentUtils {
      * 支持无限多个任务并发执行
      * @param tasks 至少传2个任务
      * @return
-     * @throws Exception
      */
     public static List<Object> concurrentExecuteDiffs(AlanTask<Object>... tasks) throws Exception {
-        return concurrentExecuteDiffs(TIMEOUT, UNIT, tasks);
+        return concurrentExecuteDiffs(TIMEOUT, UNIT, null, tasks);
+    }
+
+    /**
+     * 支持无限多个任务并发执行
+     * @param tasks 至少传2个任务
+     * @return
+     */
+    public static List<Object> concurrentExecuteDiffs(ExecutorService executorService, AlanTask<Object>... tasks) throws Exception {
+        return concurrentExecuteDiffs(TIMEOUT, UNIT, executorService, tasks);
     }
 
     /**
@@ -252,15 +312,14 @@ public class ConcurrentUtils {
      * @param unit 超时时间值的单位
      * @param tasks 至少传2个任务
      * @return
-     * @throws Exception
      */
-    public static List<Object> concurrentExecuteDiffs(int timeout, TimeUnit unit, AlanTask<Object>... tasks) throws Exception {
+    public static List<Object> concurrentExecuteDiffs(int timeout, TimeUnit unit, ExecutorService executorService, AlanTask<Object>... tasks) throws Exception {
         checkTasks(tasks);
 
         final int LEN = tasks.length;
         // 投递任务
         List<CompletableFuture<Object>> futures = Arrays.asList(tasks).stream()
-                .map( task -> CompletableFuture.supplyAsync( () -> task.execute(), getPool() ) )
+                .map( task -> CompletableFuture.supplyAsync( () -> task.execute(), executorService == null ? getPool() : executorService ) )
                 .collect(Collectors.toList());
 
         // 获取结果
@@ -279,7 +338,6 @@ public class ConcurrentUtils {
      * @param unit
      * @param <T>
      * @return
-     * @throws Exception
      */
     private static  <T> T processFuture(Future<T> f, int timeout, TimeUnit unit) throws Exception {
         try {
@@ -288,22 +346,18 @@ public class ConcurrentUtils {
         } catch (InterruptedException e) {
             e.printStackTrace();
             log.error(e.getMessage());
+
             throw new Exception(e);
 
         } catch (ExecutionException e) {
             e.printStackTrace();
+            throw new Exception(e);
 
-            // 如果是业务异常, 则重新抛出
-            if (e.getCause() instanceof Exception) {
-                throw (Exception)e.getCause();
-            } else {
-                log.error(e.getMessage());
-                throw new Exception(e);
-            }
 
         } catch (TimeoutException e) {
             e.printStackTrace();
             log.error(e.getMessage());
+
             throw new Exception(e);
         }
 
@@ -321,7 +375,7 @@ public class ConcurrentUtils {
 
     }
 
-    private static ExecutorService getPool() {
+    public static ExecutorService getPool() {
         if (null == ConcurrentUtils.pool) {
             ConcurrentUtils.pool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
             log.info("初始化默认线程池 {}, 大小: {}", ConcurrentUtils.pool, THREAD_POOL_SIZE);
@@ -331,6 +385,5 @@ public class ConcurrentUtils {
         log.debug("使用线程池: {}, 大小: {}", pool, THREAD_POOL_SIZE);
         return ConcurrentUtils.pool;
     }
-    
 
 }
